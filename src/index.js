d01430595698806ec8fa79900cba27748158e966
AdyPutra3234 <romadona.xtl4@gmail.com>
2021-02-11 17:25:15 +0700

    edit index.html, index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './script/components/navbar/navbar.js';
 import './script/components/heroComp/heroComp.js';
 import './script/components/sectionPage/section-page.js';
 import './script/components/buttonComp/buttonComp.js';
+import './script/components/footer/foot-bar.js';
 
 import './index.css';
 
