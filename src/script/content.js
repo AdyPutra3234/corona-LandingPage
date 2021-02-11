246429013676e59ed48ee4d268d48c8411a1a595
AdyPutra3234 <romadona.xtl4@gmail.com>
2021-02-11 17:24:09 +0700

    edit content.js , main.js

--- src/script/content.js
+++ src/script/content.js
@@ -1,14 +1,42 @@
 import aboutImg from '../assets/virus.jpg';
+import symptomsImg from '../assets/symptoms.jpg';
+import contagionImg from '../assets/contagion.png';
+import preventionImg from '../assets/prevention.png';
+
 
 const contentSection = [
 
     { 
         image : aboutImg,
-        section_menu : "About",
-        title : "What is Coronavirus",
-        description : "Coronaviruses are a group of related RNA viruses that cause diseases in mammals and birds. In humans and birds, they cause respiratory tract infections that can range from mild to lethal. Mild illnesses in humans include some cases of the common cold (which is also caused by other viruses, predominantly rhinoviruses), while more lethal varieties can cause SARS, MERS, and COVID-19. In cows and pigs they cause diarrhea, while in mice they cause hepatitis and encephalomyelitis.</br> </br> Coronaviruses constitute the subfamily Orthocoronavirinae, in the family Coronaviridae, order Nidovirales, and realm Riboviria. They are enveloped viruses with a positive-sense single-stranded RNA genome and a nucleocapsid of helical symmetry. The genome size of coronaviruses ranges from approximately 26 to 32 kilobases, one of the largest among RNA viruses. They have characteristic club-shaped spikes that project from their surface, which in electron micrographs create an image reminiscent of the solar corona, from which their name derives.",
-        next_section : "Country Cases"
+        section_menu : "Tentang",
+        title : "Apa itu virus corona ?",
+        description : "Coronavirus merupakan keluarga besar virus yang menyebabkan penyakit pada manusia dan hewan. Pada manusia biasanya menyebabkan penyakit infeksi saluran pernapasan, mulai flu biasa hingga penyakit yang serius seperti Middle East Respiratory Syndrome (MERS) dan Sindrom Pernafasan Akut Berat/ Severe Acute Respiratory Syndrome (SARS). Coronavirus jenis baru yang ditemukan pada manusia sejak kejadian luar biasa muncul di Wuhan Cina, pada Desember 2019, kemudian diberi nama Severe Acute Respiratory Syndrome Coronavirus 2 (SARS-COV2), dan menyebabkan penyakit Coronavirus Disease-2019 (COVID-19).",
+        next_section : "Gejala"
     },
+    {
+        image : symptomsImg,
+        section_menu : "Gejala",
+        title : "Kenali gejala covid-19 ?",
+        description : "Orang dengan penyakit covid-19 memiliki berbagai gejala mulai dari gejala ringan hingga penyakit parah <b>1 - 14 hari setelah terpapar virus</b>.",
+        list : ["Demam","Batuk kering","Sesak napas","Sakit kepala", "Kelelahan"],
+        next_section : "Penularan"
+    },
+    {
+        image : contagionImg,
+        section_menu : "Penularan",
+        title : "Bagaimana covid-19 bisa menular ?",
+        description : "Menurut World Health Organization (WHO), COVID-19 menular melalui orang yang telah terinfeksi virus corona.",
+        list : ["Kontak langsung dengan orang yang terpapar virus", "Kontak dengan benda yang terpapar virus", "Udara yang terpapar virus"],
+        next_section : "Pencegahan"
+    },
+    {
+        image : preventionImg,
+        section_menu : "Pencegahan",
+        title : "Bagaimana mencegah penularan ?",
+        description : "Lindungi diri Anda dan orang lain di sekitar Anda dengan mengetahui fakta-fakta terkait virus ini dan mengambil langkah pencegahan yang sesuai. Ikuti saran yang diberikan oleh otoritas kesehatan setempat.",
+        list : ["Cuci tangan secara rutin","Jaga jarak dengan orang lain", "Kenakan masker saat bepergian" , "jangan keluar rumah jika tidak enak badan"],
+    },
+    
 ]
 
 export default contentSection;
--- src/script/main.js
+++ src/script/main.js
@@ -3,14 +3,21 @@ import contentsPage from '../script/content.js';
 
 const main = () => {
     
-    contentsPage.forEach(content => {
+    contentsPage.forEach( content => {
     
         const mainContentWrapper = document.querySelector('.content');
         const sectionElement = document.createElement('section-page');
-        
+
         sectionElement.contents = content;
         mainContentWrapper.appendChild(sectionElement);
-    
+    });
+
+    const section_wrapper = document.querySelectorAll('.section-wrapper');
+    section_wrapper.forEach( (section, index) => {
+        if (index % 2 !== 0) {
+            section.style.background = 'white';
+            section.style.flexDirection = 'row-reverse';
+        }
     });
 }
 
