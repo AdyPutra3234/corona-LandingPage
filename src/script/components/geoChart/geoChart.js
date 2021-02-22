9868dfe430e182a268f110fb61aeb2c7a15e7e39
AdyPutra3234 <romadona.xtl4@gmail.com>
2021-02-22 13:47:54 +0700

    edit geoChart.js

--- src/script/components/geoChart/geoChart.js
+++ src/script/components/geoChart/geoChart.js
@@ -8,47 +8,70 @@ class GeoChart extends HTMLElement {
         super();
         this.Url_Api = 'https://api.kawalcorona.com/';
         this.render();
-        this.loadDataVisualization();
         
     }
 
-    loadDataVisualization() {
+    getData() {
 
-        dataSource.getData(this.Url_Api)
-            .then(dataResponse => {
+        return dataSource.getData(this.Url_Api)
+        .then(dataResponse => {
 
-                const dataTable = [['Country','Deaths']];
+            const dataTable = [['Country','Deaths']];
 
-                dataResponse.forEach( data => {
-                
-                    const region = data.attributes.Country_Region;
-                    const deathCases = data.attributes.Deaths;
+            dataResponse.forEach( data => {
+            
+                const region = data.attributes.Country_Region;
+                const deathCases = data.attributes.Deaths;
 
-                    dataTable.push([region, deathCases]);
+                dataTable.push([region, deathCases]);
 
-                });
+            });
 
-                google.charts.load('current', {
-                    'packages' : ['geochart'],
-                    // Note: you will need to get a mapsApiKey for your project.
-                    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                    'mapsApiKey' : process.env.API_KEY
-                });
-        
-                google.charts.setOnLoadCallback( () => {
+            return dataTable;
 
-                    const dataVisualization = google.visualization.arrayToDataTable(dataTable);
-                    const options = {};
-                    const chart = new google.visualization.GeoChart(document.getElementById('geo_chart'));
-                    chart.draw(dataVisualization , options);
-
-                });
         })
+
+    }
+
+    drawMapChart(data) {
+
+        return function() {
+
+            const dataVisualization = google.visualization.arrayToDataTable(data);
+            const options = {};
+            const chart = new google.visualization.GeoChart(document.getElementById('geo_chart'));
+            chart.draw(dataVisualization , options);
+
+        }
+    }
+
+    async loadDataVisualization() {
+        
+        try {
+
+            const data = await this.getData();
+
+            google.charts.load('current', {
+                'packages' : ['geochart'],
+                // Note: you will need to get a mapsApiKey for your project.
+                // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                'mapsApiKey' : process.env.API_KEY
+            });
+    
+            google.charts.setOnLoadCallback(this.drawMapChart(data));
+            
+        } catch(error) {
+
+            console.log(error);
+            
+        }
+
     }
 
     render() {
 
         this.innerHTML = `<div id="geo_chart"></div>`;
+        this.loadDataVisualization();
 
     }
 
