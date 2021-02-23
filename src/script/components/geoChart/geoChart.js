2f1eb2bd1cd7fc72ea5a9028aa93de6280054829
AdyPutra3234 <romadona.xtl4@gmail.com>
2021-02-23 20:29:01 +0700

    edit geoChart

--- src/script/components/geoChart/geoChart.js
+++ src/script/components/geoChart/geoChart.js
@@ -3,33 +3,88 @@ import './geoChart.css';
 
 class GeoChart extends HTMLElement {
 
-    constructor() {
+    connectedCallback() {
 
-        super();
-        this.Url_Api = 'https://api.kawalcorona.com/';
         this.render();
+        this.getData();
         
     }
 
+    // getData() {
+
+    //     return dataSource.getData('https://api.kawalcorona.com/')
+    //     .then(dataResponse => {
+
+    //         const dataTable = [['Country','Deaths']];
+
+    //         dataResponse.forEach( data => {
+            
+    //             const region = data.attributes.Country_Region;
+    //             const deathCases = data.attributes.Deaths;
+
+    //             dataTable.push([region, deathCases]);
+
+    //         });
+
+    //         return dataTable;
+
+    //     })
+
+    // }
+
+    // getDataGlobal() {
+    //     return dataSource.getData('https://covid.mathdro.id/api')
+    //         .then(result => {
+    //             return {
+    //                 confirmed : result.confirmed.value,
+    //                 deaths : result.deaths.value,
+    //                 recovered : result.recovered.value
+    //             }
+    //         })
+    // }
+
+    
     getData() {
 
-        return dataSource.getData(this.Url_Api)
-        .then(dataResponse => {
+        Promise.all(
+            [
+                dataSource.getData('https://covid.mathdro.id/api'),
+                dataSource.getData('https://api.kawalcorona.com/')
+            ])
+            .then(([globalCases,dataRegions]) => {
 
-            const dataTable = [['Country','Deaths']];
+                
+                const dataTable = [['Negara','Meninggal']];
+                
+                dataRegions.forEach( data => {
+                
+                    const region = data.attributes.Country_Region;
+                    const deathCases = data.attributes.Deaths;
+                    
+                    dataTable.push([region, deathCases]);
+                    
+                });
+                this.loadDataVisualizationMap(dataTable);
+                
+                document.querySelector('.data_global').innerHTML = `
+                    <div><p style="color : yellow;">${globalCases.confirmed.value}</p><p>kasus</p></div>
+                    <div><p style="color : red;">${globalCases.deaths.value}</p><p>meninggal</p></div>
+                    <div><p style="color : limegreen;">${globalCases.recovered.value}</p><p>sembuh</p></div>
+                    `;
+            })
 
-            dataResponse.forEach( data => {
-            
-                const region = data.attributes.Country_Region;
-                const deathCases = data.attributes.Deaths;
+    }
 
-                dataTable.push([region, deathCases]);
+    loadDataVisualizationMap(dataTable) {
 
-            });
-
-            return dataTable;
-
-        })
+        google.charts.load('current', {
+            'packages' : ['geochart'],
+            // Note: you will need to get a mapsApiKey for your project.
+            // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+            'mapsApiKey' : process.env.API_KEY
+        });
+    
+            google.charts.setOnLoadCallback(this.drawMapChart(dataTable));
 
     }
 
@@ -45,33 +100,13 @@ class GeoChart extends HTMLElement {
         }
     }
 
-    async loadDataVisualization() {
-        
-        try {
-
-            const data = await this.getData();
-
-            google.charts.load('current', {
-                'packages' : ['geochart'],
-                // Note: you will need to get a mapsApiKey for your project.
-                // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-                'mapsApiKey' : process.env.API_KEY
-            });
-    
-            google.charts.setOnLoadCallback(this.drawMapChart(data));
-            
-        } catch(error) {
-
-            console.log(error);
-            
-        }
-
-    }
-
     render() {
 
-        this.innerHTML = `<div id="geo_chart"></div>`;
-        this.loadDataVisualization();
+        this.innerHTML = `
+        <div class="data_visualization">
+            <div class="data_global"></div>
+            <div id="geo_chart"></div>
+        </div>`;
 
     }
 
